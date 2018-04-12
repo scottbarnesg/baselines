1319712f8eaa9c9601040704c4e1516ef29da828
scottbarnesg <scottgbarnes@gwu.edu>
2018-04-12 15:49:57 -0400

    Vectorized ParticleEnv Compatability

--- experiments/run_particleEnvs.py
+++ experiments/run_particleEnvs.py
@@ -21,17 +21,76 @@ from baselines.a2c.policies import CnnPolicy, LstmPolicy, LnLstmPolicy, MlpPolic
 from baselines.a2c.utils import fc, conv, conv_to_fc, sample
 
 
-class envVec(VecEnv):
-    def __init__(self, env_fns):
-        self.envs = [fn() for fn in env_fns]
+class EnvVec(VecEnv):
+    def __init__(self, env_fns, particleEnv):
+        # print(env_fns)
+        # self.envs = [fn() for fn in env_fns]
+        self.envs = env_fns
         env = self.envs[0]
         self.action_space = env.action_space
         self.observation_space = env.observation_space
+        self.ts = np.zeros(len(self.envs), dtype='int')
+        self.remotes = [0]*len(env_fns)
+        self.n = env.n
+        self.particleEnv=particleEnv
+        print('Successfully Loaded Particle_Env Environments')
+        print('----------------------------------------------')
+
+    def step(self, action_n, ind=0):
+        obs = []
+        rews = []
+        dones = []
+        infos = []
+        imgs = []
+        # print('action_n = ' + str(action_n))
+        # print(action_n)
+        if self.particleEnv == False:
+            for (a,env) in zip(action_n, self.envs): # REPLACE
+                ob, rew, done, info = env.step(a, ind) # MAY NOT BE CORRECT
+        else:
+            # print(self.num_envs)
+            for i in range(self.num_envs):
+                # print(i)
+                env = self.envs[i]
+                a = [[action_n[0][0][0][i], action_n[0][0][1][i]], [action_n[1][0][0][i], action_n[1][0][1][i]]]
+                # print('a: ', a)
+                ob, rew, done, info = env.step(a)
+            # plt.imshow(ob)
+            # plt.draw()
+            # plt.pause(0.000001)
+            # Need to fix below. What is the difference between obs and imgs?
+                obs.append(ob)
+                rews.append(rew)
+                dones.append(done)
+                infos.append(info)
+                imgs.append(ob)
+        self.ts += 1
+        for (i, done) in enumerate(dones):
+            # print ('Debug')
+            # print('dones ' + str(dones))
+            # print('envs' + str(self.envs))
+            # print()
+            if done:
+                # print (i)
+                # obs[i] = self.envs[i].reset()
+                self.ts[i] = 0
+        return np.array(imgs), np.array(rews), np.array(dones), infos
+
+    def reset(self):
+        results = []
+        for env in self.envs:
+            obs = env.reset()
+            results.append(obs)
+        #results = [env.reset() for env in self.envs]
+        return np.array(results)
+
+    @property
+    def num_envs(self):
+        return len(self.envs)
 
 
 
-
-def make_env(scenario_name, benchmark=False):
+def make_env(scenario_name, benchmark=False, rank=-1, seed=0):
     from multiagent.environment import MultiAgentEnv
     import multiagent.scenarios as scenarios
 
@@ -45,12 +104,14 @@ def make_env(scenario_name, benchmark=False):
         env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, scenario.benchmark_data)
     else:
         env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
+    env.seed = (seed+rank)
+    env.ID = rank
     return env
 
 def train(env_id, num_timesteps, seed, policy, lrschedule, num_cpu, continuous_actions=False, numAgents=2, benchmark=False):
     # Create environment
-    # env = envVec([make_env(idx, benchmark) for idx in range(num_cpu)])
-    env = make_env(env_id, benchmark)
+    env = EnvVec([make_env(env_id, benchmark=benchmark, rank=idx, seed=seed) for idx in range(num_cpu)], particleEnv=True)
+    # env = make_env(env_id, benchmark)
     # print('action space: ', env.action_space)
     # env = GymVecEnv([make_env(idx) for idx in range(num_cpu)])
     policy_fn = policy_fn_name(policy)
@@ -96,4 +157,4 @@ if __name__ == '__main__':
         test(args.env, args.policy, args.seed, nstack=1)
     else:
         train(args.env, num_timesteps=args.num_timesteps, seed=args.seed,
-              policy=args.policy, lrschedule=args.lrschedule, num_cpu=1, continuous_actions=continuous_actions, numAgents=numAgents)
+              policy=args.policy, lrschedule=args.lrschedule, num_cpu=4, continuous_actions=continuous_actions, numAgents=numAgents)
